90079262e01bd9d516a83240a2f9520bf680b847
mimiisaak <mimiisaak2@gmail.com>
2021-09-29 16:57:35 +0300

    cities schema

---
+++ DeliveryManagement‏/backend/Model/CitiesModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const cities = new Schema(
+    {
+        name: {
+            type: String
+        },
+    },
+    { collection: "cities" }
+);
+module.exports = mongoose.model("cities", cities);
